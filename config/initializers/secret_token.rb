32c9a5c9cf94479779ca6e74333c60661bb2153b
lwangenheim <lwangenheim@gmail.com>
2014-02-27 14:28:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GotchaQuiz::Application.config.secret_key_base = '9ee32091df375b53d5e3b530af30f03f5025653c6f6bd4855796b5a8608b7772744f42baf256626d3e4805ea089ae2945bd88043a66f5766a41c2265e508158f'

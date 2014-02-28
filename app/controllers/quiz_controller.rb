cef51f8fc6803c1c5f05aedff47986b20d14d31a
lwangenheim <lwangenheim@gmail.com>
2014-02-27 19:22:40 -0500

    simple form

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,5 +1,10 @@
 class QuizController < ApplicationController
-  def start
-    
-  end
+  # def start
+  #   
+  # end
+  
+  def check
+      @quiz = Quiz.find(params[:id])
+        @correct = (params[:guess] == @quiz.correct_answer)
+  end 
 end
